0f87854946c6b411364766c0cb2e9a0074e2bd49
Ansh Kataria <ansh.kataria@student.uq.edu.au>
2026-01-18 07:05:07 +0530

    add postgres in application properties

---
+++ src/main/java/com/ansh/atomic_book/AtomicBookApplication.java
@@ -0,0 +1,13 @@
+package com.ansh.atomic_book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtomicBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtomicBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ansh/atomic_book/AtomicBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ansh.atomic_book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtomicBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
