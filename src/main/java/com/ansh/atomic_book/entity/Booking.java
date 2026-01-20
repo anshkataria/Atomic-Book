746a243e834a1bcd2dbcd9b99d517abfea7d0aa7
Ansh Kataria <ansh.kataria@student.uq.edu.au>
2026-01-20 06:58:42 +0530

    add booking entity

--- src/main/java/com/ansh/atomic_book/entity/Booking.java
+++ src/main/java/com/ansh/atomic_book/entity/Booking.java
@@ -1,4 +1,69 @@
 package com.ansh.atomic_book.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Entity
+@Table(name = "bookings")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Booking {
+
+    private enum BookingStatus {
+        CONFIRMED,
+        PENDING,
+        FAILED,
+        CANCELLED
+    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="user_id",nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="event_id",nullable = false)
+    private Event event;
+
+    @Column(nullable = false)
+    private Integer numberOfTicketsBooked;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private BookingStatus bookingStatus = BookingStatus.PENDING;
+
+    @Column(nullable = false)
+    private BigDecimal totalBookingPrice;
+
+    @Column(unique = true, nullable = false)
+    private String bookingReference;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime bookingDate;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @OneToOne(mappedBy = "booking", cascade = CascadeType.ALL)
+    private List<Ticket> tickets = new ArrayList<>();
+
+
+
+
+
 }
