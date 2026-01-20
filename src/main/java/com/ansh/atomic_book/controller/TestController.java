c65891cb88179dcbe7a95effc7d9918b52a62f01
Ansh Kataria <ansh.kataria@student.uq.edu.au>
2026-01-20 07:15:51 +0530

    add test controller to check the health

---
+++ src/main/java/com/ansh/atomic_book/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.ansh.atomic_book.controller;
+
+import com.ansh.atomic_book.entity.Booking;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+
+    @GetMapping("/health")
+    public Map<String, Object> healthCheck() {
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", "OK");
+        response.put("message", "Atomic Book API is up and running");
+        return response;
+    }
+}
