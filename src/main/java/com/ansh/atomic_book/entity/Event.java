39c6d3aec66d8261307a018acedac4584abea74f
Ansh Kataria <ansh.kataria@student.uq.edu.au>
2026-01-20 06:51:06 +0530

    add event entity

--- src/main/java/com/ansh/atomic_book/entity/Event.java
+++ src/main/java/com/ansh/atomic_book/entity/Event.java
@@ -1,4 +1,73 @@
 package com.ansh.atomic_book.entity;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "events")
 public class Event {
+
+    private enum Category {
+        MOVIE,
+        CONCERT,
+        SPORTS,
+        WORKSHOP,
+        THEATER,
+        OTHER
+    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+    @Column(length = 2000)
+    private String description;
+
+    @Column(nullable = false)
+    private String venue;
+
+    @Column(nullable = false)
+    private LocalDateTime eventDateTime;
+
+    @Column(nullable = false)
+    private Integer totalSeats;
+
+    @Column(nullable = false)
+    private Integer availableSeats;
+
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    private String imageUrl;
+
+    @Column(nullable = false)
+    private Boolean isActive = true;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @OneToMany(mappedBy = "event",cascade = CascadeType.ALL)
+    private List<Booking> bookings = new ArrayList<>();
+
 }
